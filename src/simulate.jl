9a6c17dacbdf9b21b1e306ef2e963d0cd8f123ab
Timothée Poisot <tim@poisotlab.io>
2016-02-04 14:54:40 -0500

    Simulate returns a Dict

--- src/simulate.jl
+++ src/simulate.jl
@@ -2,5 +2,14 @@ function simulate(p, biomass; start=0, stop=500, steps=10)
     t = collect(linspace(start, stop, stop * steps))
     f(t, y, ydot) = dBdt(t, y, ydot, p)
     timeseries = Sundials.cvode(f, biomass, t)
+
+    output = Dict{Symbol,Any}(
+        :p => p,
+        :t => t,
+        :B => timeseries
+    )
+
+    return output
+
 end
 
