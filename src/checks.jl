ed72c5fe781ea1370ce99c10fd4b674733ee1911
Timothée Poisot <tim@poisotlab.io>
2016-02-04 13:56:36 -0500

    Untested functions to check parameters

--- src/checks.jl
+++ src/checks.jl
@@ -7,3 +7,44 @@ function check_food_web(A)
     @assert length(size(A)) == 2
     @assert sum(map(x -> x ∉ [0 1], A)) == 0
 end
+
+function check_initial_parameters(p)
+    required_keys = [
+        :Z,
+        :vertebrates,
+        :a_vertebrate,
+        :a_invertebrate,
+        :a_producer,
+        :m_producer,
+        :y_vertberate,
+        :y_invertebrate,
+        :e_herbivore,
+        :e_carnivore,
+        :h,
+        :Γ,
+        :c,
+        :r,
+        :K
+    ]
+    for k in required_keys
+        @assert get(p, k, nothing) != nothing
+    end
+end
+
+function check_parameters(p)
+    # Users need only call this function
+    check_initial_parameters(p)
+
+    required_keys = [
+        :w,
+        :efficiency,
+        :y,
+        :x,
+        :a,
+        :is_herbivore,
+        :is_producer
+    ]
+    for k in required_keys
+        @assert get(p, k, nothing) != nothing
+    end
+end
