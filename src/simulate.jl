ac7a10163c25c5818edde8e0c32fd744b996f49d
Timothée Poisot <tim@poisotlab.io>
2016-02-04 21:56:15 -0500

    Can pass reltol and abstol to Sundials

--- src/simulate.jl
+++ src/simulate.jl
@@ -1,9 +1,9 @@
-function simulate(p, biomass; start::Int64=0, stop::Int64=500, steps::Int64=10)
+function simulate(p, biomass; start::Int64=0, stop::Int64=500, steps::Int64=100, reltol::Float64=1e-8, abstol::Float64=1e-16)
     t_nsteps = (stop - start + 1)
     nsteps = steps * t_nsteps + t_nsteps
     t = collect(linspace(start, stop, nsteps))
     f(t, y, ydot) = dBdt(t, y, ydot, p)
-    timeseries = Sundials.cvode(f, biomass, t)
+    timeseries = Sundials.cvode(f, biomass, t, reltol, abstol)
 
     # Because small timesteps are sometimes needed, the output can get big
     # As in, several GB per simulation
