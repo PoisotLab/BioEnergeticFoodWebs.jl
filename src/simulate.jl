786f2ecd3d3535f775f304ea7bad127f28bec742
Timothée Poisot <tim@poisotlab.io>
2016-02-04 19:24:09 -0500

    Only keep records for .0 timesteps

--- src/simulate.jl
+++ src/simulate.jl
@@ -3,10 +3,17 @@ function simulate(p, biomass; start=0, stop=500, steps=10)
     f(t, y, ydot) = dBdt(t, y, ydot, p)
     timeseries = Sundials.cvode(f, biomass, t)
 
+    # Because small timesteps are sometimes needed, the output can get big
+    # As in, several GB per simulation
+    # So we'll record only every timestep
+
+    t_collect = collect(linspace(start, stop, stop - start + 1))
+    t_keep = [x ∈ t_collect for x in t]
+
     output = Dict{Symbol,Any}(
         :p => p,
-        :t => t,
-        :B => timeseries
+        :t => t[t_keep],
+        :B => timeseries[t_keep,:]
     )
 
     return output
