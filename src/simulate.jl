0ba846a57f48e38abe5186551d43d6122f5ee95b
Timothée Poisot <tim@poisotlab.io>
2016-02-09 14:32:32 -0500

    Chunks for simulate
    Simulate is executed by chunks of 2000 timesteps. Not as thoroughly
    tested as it should. Yet.

--- src/simulate.jl
+++ src/simulate.jl
@@ -31,31 +31,80 @@ keys:
 
 The array of biomasses has one row for each timestep, and one column for
 each species.
+
+If the difference between stop and start is more than an arbitrary threshold
+(currently 2000 timesteps), the simulations will be run in chunks of 2000
+timesteps each. This is because the amount of memory needed to store the
+dynamics scales very badly. To avoid `OutOfMemory()` errors, running the
+simulation by parts is sufficient.
+
 """
 function simulate(p, biomass; start::Int64=0, stop::Int64=500, steps::Int64=5000)
     @assert stop > start
     @assert steps > 1
     @assert length(biomass) == size(p[:A],1)
-    t_nsteps = (stop - start + 1)
-    nsteps = steps * t_nsteps + t_nsteps
-    t = collect(linspace(start, stop, nsteps))
-    f(t, y, ydot) = dBdt(t, y, ydot, p)
-    timeseries = Sundials.cvode(f, biomass, t)
 
-    # Because small timesteps are sometimes needed, the output can get big
-    # As in, several GB per simulation
-    # So we'll record only every timestep
+    # Pre-allocate the timeseries matrix
+    t = collect(linspace(start, stop, (stop-start)+1))
+    nt, ns = length(t), length(biomass)
+    timeseries = zeros((nt, ns))
 
-    t_collect = collect(linspace(start, stop, t_nsteps))
-    t_keep = [x ∈ t_collect for x in t]
+    # We put the starting conditions in the array
+    timeseries[1,:] = biomass
+
+    chunk_size = 2000
+    done_up_to = start
+    while done_up_to < stop
+        start_at = done_up_to
+        stop_at = start_at + chunk_size
+        if stop_at > stop
+            stop_at = stop
+        end
+        i = start_at-start + 1
+        inner_simulation_loop!(timeseries, p, i, start=start_at, stop=stop_at, steps=steps)
+        done_up_to = stop_at
+    end
 
     output = Dict{Symbol,Any}(
         :p => p,
-        :t => t[t_keep],
-        :B => timeseries[t_keep,:]
+        :t => t,
+        :B => timeseries
     )
 
     return output
 
+end
+
+"""
+**Inner simulation loop**
+
+This function is called internally by `simulate`, and should not be called
+by the user.
+
+Note that `output` is a pre-allocated array in which the simulation result
+will be written, and `i` is the origin of the simulation.
+
+"""
+function inner_simulation_loop!(output, p, i; start::Int64=0, stop::Int64=2000, steps::Int64=5000)
+    
+    t_nsteps = (stop - start + 1)
+    nsteps = (stop - start) * steps + 1
+    t = collect(linspace(start, stop, nsteps))
+    f(t, y, ydot) = dBdt(t, y, ydot, p)
+
+    # Read the biomass in the pre-allocated array
+    biomass = vec(output[i,:])
+
+    # Integrate
+    timeseries = Sundials.cvode(f, biomass, t)
+
+    # Get only the int times
+    t_collect = collect(linspace(start, stop, t_nsteps))
+    t_keep = [x ∈ t_collect for x in t]
+
+    ok_indices = collect(i:(i+sum(t_keep)-1))
+    
+    # Update the output array
+    output[ok_indices,:] = timeseries[t_keep,:]
 end
 
