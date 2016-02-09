5a756b299ed2bac806c4abcbfdf9c60c3a5a853f
Timothée Poisot <tim@poisotlab.io>
2016-02-08 21:02:53 -0500

    Test typo

--- test/make_parameters.jl
+++ test/make_parameters.jl
@@ -4,7 +4,7 @@ module TestMakeParameters
 
     # Test the keyword interface
     correct_network = [0 0 1 1; 1 1 0 0; 0 1 0 1; 1 1 0 0]
-    p = make_initial_parameters(A, Z=2.0)
+    p = make_initial_parameters(correct_network, Z=2.0)
     @test p[:Z] == 2.0
     
 
