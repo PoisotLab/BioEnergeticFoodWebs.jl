ee5d3cb88be89d7da2348b16ca51089f805c23d6
Timothée Poisot <tim@poisotlab.io>
2016-02-08 21:26:28 -0500

    Test update

--- test/make_parameters.jl
+++ test/make_parameters.jl
@@ -7,9 +7,4 @@ module TestMakeParameters
     p = make_initial_parameters(correct_network, Z=2.0)
     @test p[:Z] == 2.0
 
-    # Test if only a network is passed
-    p2 = make_parameters(correct_network)
-    @test p[:Z] == 1.0
-    
-
 end
