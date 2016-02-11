d6f97b73850734286443ba9f35ea48f03a8d7a4a
Timothée Poisot <tim@poisotlab.io>
2016-02-11 12:19:22 -0500

    Documentation upodate

--- src/measures.jl
+++ src/measures.jl
@@ -1,9 +1,25 @@
+"""
+**Coefficient of variation**
+
+Corrected for the sample size.
+
+"""
 function coefficient_of_variation(x)
     cv = std(x) / mean(x)
     norm = 1 + 1 / (4 * length(x))
     return norm * cv
 end
 
+"""
+**Population stability**
+
+Takes a matrix with populations in columns, timesteps in rows. This is usually
+the element `:B` of the simulation output. Population stability is measured
+as the mean of the negative coefficient of variations of all species with
+an abundance higher than `threshold`. By default, the stability is measure
+over the last `last=1000` timesteps.
+
+"""
 function population_stability(D; threshold=1e-10, last=1000)
     @assert last <= size(A, 1)
     non_extinct = D[end,:] .> threshold
