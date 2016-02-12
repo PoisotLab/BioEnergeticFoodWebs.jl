4b2c466110c3c294b9a5974e031e8753428b1a22
Timothée Poisot <tim@poisotlab.io>
2016-02-12 15:35:44 -0500

    Test suite update

--- test/trophic_rank.jl
+++ test/trophic_rank.jl
@@ -9,14 +9,14 @@ module TestTrophicRank
     @assert trophic_rank(food_chain) == vec([3.0 2.0 1.0])
 
     omnivory = [0 1 1; 0 0 1; 0 0 0]
-    @assert trophic_rank(omnivory) == vec([3.0 2.0 1.0])
+    @assert trophic_rank(omnivory) == vec([2.5 2.0 1.0])
 
     diamond = [0 1 1 0; 0 0 0 1; 0 0 0 1; 0 0 0 0]
     @assert trophic_rank(diamond) == vec([3.0 2.0 2.0 1.0])
 
     chain_four = [0 1 0 0; 0 0 1 0; 0 0 0 1; 0 0 0 0]
-    @assert trophic_rank(chain_four) == vec([3.0 3.0 2.0 1.0])
+    @assert trophic_rank(chain_four) == vec([4.0 3.0 2.0 1.0])
 
-    @assert trophic_rank(zeros((3, 3))) == ones(3)
+    @assert trophic_rank(zeros(Int64, (3, 3))) == ones(3)
 
 end
