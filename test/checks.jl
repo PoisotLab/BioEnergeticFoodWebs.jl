731dd4feedb0a49dfbdd521fba6e9c3c33677456
Timothée Poisot <tim@poisotlab.io>
2016-02-19 11:52:00 -0500

    Test update

--- test/checks.jl
+++ test/checks.jl
@@ -4,14 +4,14 @@ module TestCheckFoodWeb
 
     # A correct network will return nothing
     correct_network = [0 0 1 1; 1 1 0 0; 0 1 0 1; 1 1 0 0]
-    @test check_food_web(correct_network) == nothing
+    @test befwm.check_food_web(correct_network) == nothing
 
     # A non-square network will fail
     non_square = [0 0 1; 0 1 1; 0 1 0; 1 0 0]
-    @test_throws AssertionError check_food_web(non_square)
+    @test_throws AssertionError befwm.check_food_web(non_square)
 
     # A network with antyhing else than 0/1 will fail
     non_binary = [0 0 1 1; 1 1 0 0; 0 1 0 1; 1 1 2 0]
-    @test_throws AssertionError check_food_web(non_binary)
+    @test_throws AssertionError befwm.check_food_web(non_binary)
 
 end
