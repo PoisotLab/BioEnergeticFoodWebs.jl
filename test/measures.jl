01502dcb9836df419314c321c190b5700870aa15
Timothée Poisot <tim@poisotlab.io>
2016-02-19 13:18:39 -0500

    Fix test suite

--- test/measures.jl
+++ test/measures.jl
@@ -1,12 +1,12 @@
-module TestTrophicRank
+module TestMeasures
     using Base.Test
     using befwm
 
     i = ones(10)
     @test_approx_eq befwm.shannon(i) 1.0
 
-    i = zeros(100).+0,001
+    i = zeros(100).+0.001
     i[1] = 1.0
-    @test_approx_eq_eps befwm.shannon(i) 0.0 0.11
+    @test_approx_eq_eps befwm.shannon(i) 0.0 0.2
 
 end
--- test/random.jl
+++ test/random.jl
@@ -1,10 +1,15 @@
-module TestTrophicRank
+module TestRandom
     using Base.Test
     using befwm
 
     @test_throws AssertionError nichemodel(10, 150)
     
-    A = nichemodel(10, 50)
-    @assert size(A) == (10, 10)
+    A = nichemodel(10, 20)
+    @test size(A, 1) == 10
+    @test size(A, 2) == 10
+
+    A = nichemodel(10, 0.12)
+    @test size(A, 1) == 10
+    @test size(A, 2) == 10
 
 end
