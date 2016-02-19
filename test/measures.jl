0de1487ebb7e780f6c53d24b59b2a1b2cbc506c9
Timothée Poisot <tim@poisotlab.io>
2016-02-19 13:41:40 -0500

    Test coeff var

--- test/measures.jl
+++ test/measures.jl
@@ -9,4 +9,11 @@ module TestMeasures
     i[1] = 1.0
     @test_approx_eq_eps befwm.shannon(i) 0.0 0.2
 
+
+    i = ones(5)
+    @test befwm.coefficient_of_vairation(i) == 0.0
+
+    i = collect(linspace(0.0, 1.0, 3))
+    @test_approx_eq befwm.coefficient_of_variation(i) 1+1/(4*length(i))
+
 end
