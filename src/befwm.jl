56cdaddd6631dbd30fd0861a0197d0541c8477f0
Timothée Poisot <tim@poisotlab.io>
2016-05-04 23:50:00 -0400

    Indentation

--- src/befwm.jl
+++ src/befwm.jl
@@ -1,28 +1,28 @@
 module befwm
 
-    using Sundials
-    using Distributions
-    using ODE
-    using JSON
-    using JLD
+using Sundials
+using Distributions
+using ODE
+using JSON
+using JLD
 
-    export trophic_rank,
-        make_parameters,
-        make_initial_parameters,
-        simulate,
-        nichemodel,
-        population_stability,
-        total_biomass,
-        population_biomass,
-        foodweb_diversity
+export trophic_rank,
+  make_parameters,
+  make_initial_parameters,
+  simulate,
+  nichemodel,
+  population_stability,
+  total_biomass,
+  population_biomass,
+  foodweb_diversity
 
-    # Includes
-    include(joinpath(".", "trophic_rank.jl"))
-    include(joinpath(".", "checks.jl"))
-    include(joinpath(".", "dBdt.jl"))
-    include(joinpath(".", "make_parameters.jl"))
-    include(joinpath(".", "simulate.jl"))
-    include(joinpath(".", "random.jl"))
-    include(joinpath(".", "measures.jl"))
+# Includes
+include(joinpath(".", "trophic_rank.jl"))
+include(joinpath(".", "checks.jl"))
+include(joinpath(".", "dBdt.jl"))
+include(joinpath(".", "make_parameters.jl"))
+include(joinpath(".", "simulate.jl"))
+include(joinpath(".", "random.jl"))
+include(joinpath(".", "measures.jl"))
 
 end
