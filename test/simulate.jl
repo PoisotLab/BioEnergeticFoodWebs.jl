bdb4d1ef71473a7a739b9047e8f8bd235365cc63
Timothée Poisot <tim@poisotlab.io>
2016-06-21 00:20:31 -0400

    Update code

--- test/simulate.jl
+++ test/simulate.jl
@@ -3,8 +3,7 @@ module TestSimulate
     using befwm
 
     food_chain = [0 0 0;0 0 1; 1 0 0]
-    p = make_initial_parameters(food_chain)
-    p = make_parameters(p)
+    p = model_parameters(food_chain)
     # Fail if the biomass vector has bad length
     @test_throws AssertionError simulate(p, rand(5))
     # Fail if stop < start
@@ -19,8 +18,7 @@ module TestSimulateHandChecked
     using befwm
 
     A = [0 1 0; 0 0 0; 0 1 0]
-    p = make_initial_parameters(A)
-    p = make_parameters(p)
+    p = model_parameters(A)
     b0 = vec([0.2 0.4 0.1])
     der = zeros(Float64, 3)
     der = befwm.dBdt(0.0, b0, der, p)
@@ -36,7 +34,7 @@ module TestSimulateSanityCheck
 
     # A producer with no predation reaches K
     free_producers = [0 1 0 0; 0 0 1 0; 0 0 0 0; 0 0 0 0]
-    p = free_producers |> make_initial_parameters |> make_parameters
+    p = free_producers |> model_parameters
     n = rand(4)
 
     s = simulate(p, n, start=0, stop=15, steps=500)
@@ -59,7 +57,7 @@ module TestSimulateSanityCheck
     
     # A consumer with a resource with 0 biomass goes extinct
     A = [0 1; 0 0]
-    p = A |> make_initial_parameters |> make_parameters
+    p = A |> model_parameters
     n = vec([1.0, 0.0])
     s = simulate(p, n, start=0, stop=50, steps=500)
     @test_approx_eq_eps s[:B][end,1] 0.0 0.001
