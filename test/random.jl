5a2c68af68b08b251bc3a1d4870abbf2f5747164
Timothée Poisot <tim@poisotlab.io>
2016-08-14 20:26:19 -0400

    update some tests to run the connectance with the two toltype schemes

--- test/random.jl
+++ test/random.jl
@@ -3,7 +3,7 @@ module TestRandom
     using befwm
 
     @test_throws AssertionError nichemodel(10, 150)
-    
+
     A = nichemodel(10, 20)
     @test size(A, 1) == 10
     @test size(A, 2) == 10
@@ -11,6 +11,9 @@ module TestRandom
     A = nichemodel(10, 0.12)
     @test size(A, 1) == 10
     @test size(A, 2) == 10
+
+    A = nichemodel(10, 0.12, toltype=:rel)
+    A = nichemodel(10, 0.12, toltype=:abs)
 
     A = [0 1; 1 0]
     @test befwm.connectance(A) == 0.5
