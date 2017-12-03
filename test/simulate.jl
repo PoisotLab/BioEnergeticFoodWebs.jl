06d0a46e51fa37217d09a8c64bd819d31eb4096b
Eva Delmas <edelmas@localhost.localdomain>
2017-12-03 16:53:04 -0500

    :white_check_mark: New tests for simulations.

--- test/simulate.jl
+++ test/simulate.jl
@@ -33,7 +33,7 @@ module TestSimulateSanityCheck
   p = free_producers |> model_parameters
   n = rand(4)
 
-  s = simulate(p, n, start=0, stop=25, use=:nonstiff)
+  s = simulate(p, n, start=0, stop=25)
   @test s[:B][26,4] ≈ p[:K] atol=0.002
 
   # Using system-wide regulation, producers with no consumption reach K / n
@@ -52,10 +52,19 @@ module TestSimulateSanityCheck
   # @test_approx_eq_eps s[:B][end,1] 0.0 0.001
   # @test_approx_eq_eps s[:B][end,2] 0.0 0.001
 
-  s = simulate(p, n, start=0, stop=50, use=:nonstiff)
+  s = simulate(p, n, start=0, stop=50)
   @test s[:B][end,1] ≈ .0 atol=0.001
   @test s[:B][end,2] ≈ 0.0 atol=0.001
 
+  #test that stiff and nonstiff methods yield similar results
+  A = [0 0 1 ; 0 0 1 ; 0 0 0]
+  p = model_parameters(A)
+  b = [.5, .5, .5]
+  s_stiff = simulate(p, b, use = :stiff)
+  meanB_stiff = mean(s_stiff[:B], 1)
+  s_nonstiff = simulate(p, b, use = :nonstiff)
+  meanB_nonstiff = mean(s_stiff[:B], 1)
+  @test meanB_stiff == meanB_nonstiff
 end
 
 
@@ -97,5 +106,38 @@ module TestSimulateProductivity
   p = model_parameters(A, productivity=:competitive, α=0.0)
   s = simulate(p, ones(4), start=0, stop=15)
   @test s[:B][16,4] ≈ p[:K] atol=0.001
+
+end
+
+module TestSimulateRewiring
+  using Base.Test
+  using BioEnergeticFoodWebs
+
+
+  #print info message when extinction occurs
+  A = [0 0 0; 0 1 0; 0 0 0]
+  A_after = Int.(zeros(A))
+  b = rand(3)
+
+  p = model_parameters(A, rewire_method = :Gilljam)
+  @test_warn "INFO: extinction of species 2" simulate(p, b)
+  @test p[:A] == A_after
+  p = model_parameters(A, rewire_method = :stan)
+  @test_warn "INFO: extinction of species 2" simulate(p, b)
+  @test_warn "INFO: extinction of species 2" simulate(p, b)
+  @test p[:A] == A_after
+  p = model_parameters(A, rewire_method = :ADBM)
+  @test_warn "INFO: extinction of species 2" simulate(p, b)
+  @test_warn "INFO: extinction of species 2" simulate(p, b)
+  @test p[:A] == A_after
+
+  #don't print info message when rewire_method = :none, even when an extinction occurs
+  p = model_parameters(A)
+  @test_nowarn simulate(p, b)
+  #don't print info message when rewire_method != :none but no extinction occurs
+  A = [0 0 0 ; 0 0 0 ; 0 0 0] #free producers
+  p = model_parameters(A, rewire_method = :stan)
+  b = rand(3)
+  @test_nowarn simulate(p, b)
 
 end
