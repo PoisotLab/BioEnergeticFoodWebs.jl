358e73a72d88555e3ed98d8aa2e0f2f40add80c1
Azenor Bideault <azenor.bideault@usherbrooke.ca>
2018-07-16 19:06:42 -0400

    change body mass + define parameters in temperature_parameters

--- src/temperature_dependence_functions.jl
+++ src/temperature_dependence_functions.jl
@@ -15,15 +15,15 @@ In each case, the function returns the biological rate value at a given temperat
 
 **Option 1 : Extended Eppley function**
 
-| Parameter | Meaning                                                           |
-|:----------|:------------------------------------------------------------------|
-| temp      | temperature range (Celsius degree)                                |
-| a         | parameter from the Eppley curve for the growth phase              |
-| b         | parameter from the Eppley curve for the growth phase              |
-| z         | location of the maximum of the quadratic portion of the function  |
-| w         | thermal breadth                                                   |
-| m         | body mass                                                         |
-| beta      | allometric exponent                                               |
+| Parameter    | Meaning                                                           |
+|:-------------|:------------------------------------------------------------------|
+| temp         | temperature range (Celsius degree)                                |
+| a            | parameter from the Eppley curve for the growth phase              |
+| b            | parameter from the Eppley curve for the growth phase              |
+| z            | location of the maximum of the quadratic portion of the function  |
+| w            | thermal breadth                                                   |
+| p[:bodymass] | body mass                                                         |
+| beta         | allometric exponent                                               |
 
 
 No temperature effect :
@@ -32,7 +32,7 @@ a=1
 b=0
 z=18
 w=35
-m=1
+p[:bodymass]=1
 beta=-0.25
 
 To account for a temperature effect, the parameters are for instance :
@@ -40,42 +40,41 @@ To account for a temperature effect, the parameters are for instance :
     b=0.0631 (parameters from the Eppley curve)
     z=18
     w=35
-    m=1
+    p[:bodymass]=1
     beta=-0.25
 """
 
-function extended_eppley(para_extended_eppley)
+function extended_eppley(para_extended_eppley, p)
     temp=para_extended_eppley[1]
     a=para_extended_eppley[2]
     b=para_extended_eppley[3]
     z=para_extended_eppley[4]
     w=para_extended_eppley[5]
-    m=para_extended_eppley[6]
-    beta=para_extended_eppley[7]
+    beta=para_extended_eppley[6]
 
-    return m^beta*a.*exp.(b.*temp).*(1-((temp-z)./(w/2)).^2)
+    return p[:bodymass]^beta*a.*exp.(b.*temp).*(1-((temp-z)./(w/2)).^2)
 
 end
 
 """
 **Option 2 : Quadratic function**
 
-| Parameter | Meaning                                                                                  |
-|:----------|:-----------------------------------------------------------------------------------------|
-| temp      | temperature range (Kelvin)                                                               |
-| b         | fitted parameter from Englund et al (2011), determines the shape of the thermal response |
-| q         | fitted parameter from Englund et al (2011), determines the shape of the thermal response |
-| c         | fitted parameter from Englund et al (2011)                                               |
-| m         | body mass                                                                                |
-| beta      | allometric exponent                                                                      |
-| k         | Boltzmann constant (k=8.617e-5)                                                          |                                                            |
+| Parameter    | Meaning                                                                                  |
+|:-------------|:-----------------------------------------------------------------------------------------|
+| temp         | temperature range (Kelvin)                                                               |
+| b            | fitted parameter from Englund et al (2011), determines the shape of the thermal response |
+| q            | fitted parameter from Englund et al (2011), determines the shape of the thermal response |
+| c            | fitted parameter from Englund et al (2011)                                               |
+| p[:bodymass] | body mass                                                                                |
+| beta         | allometric exponent                                                                      |
+| k            | Boltzmann constant (k=8.617e-5)                                                          |                                                            |
 
 Parameters are for instance (parametrization to revise):
 
 c=0.2
 q=-0.5
 b=-1.6537077869328072e6
-m=1
+p[:bodymass]=1
 beta=-0.25
 """
 
@@ -84,24 +83,23 @@ function quadratic(para_quadratic)
     c=para_quadratic[2]
     b=para_quadratic[3]
     q=para_quadratic[4]
-    m=para_quadratic[5]
-    beta=para_quadratic[6]
+    beta=para_quadratic[5]
     k=8.617e-5
-    return m^beta*c*exp(b*(-1/k*temp)+q*(-1/k*temp)^2)
+    return p[:bodymass]^beta*c*exp(b*(-1/k*temp)+q*(-1/k*temp)^2)
 
 end
 
 """
 **Option 3 : Exponential Boltzmann-Arrhenius function**
 
-| Parameter | Meaning                               |
-|:----------|:--------------------------------------|
-| temp      | temperature range (Kelvin)            |
-| p0        | scaling coefficient                   |
-| E         | activation energy                     |
-| m         | body mass                             |
-| beta      | allometric exponent                   |
-| k         | Boltzmann constant (k=8.617e-5)       |
+| Parameter    | Meaning                               |
+|:-------------|:--------------------------------------|
+| temp         | temperature range (Kelvin)            |
+| p0           | scaling coefficient                   |
+| E            | activation energy                     |
+| p[:bodymass] | body mass                             |
+| beta         | allometric exponent                   |
+| k            | Boltzmann constant (k=8.617e-5)       |
 
 Parameters are for instance:
 
@@ -112,32 +110,31 @@ beta=-0.25
 
 """
 
-function exponential_BA(para_exponential_BA)
+function exponential_BA(para_exponential_BA,p)
     temp=para_exponential_BA[1]
     p0=para_exponential_BA[2]
     E=para_exponential_BA[3]
-    m=para_exponential_BA[4]
-    beta=para_exponential_BA[5]
+    beta=para_exponential_BA[4]
 
     k=8.617e-5 # Boltzmann constant
 
-    return p0*m^beta*exp(-E/(k*temp))
+    return p0.*p[:bodymass].^beta.*exp.(-E./(k*temp))
 
 end
 
 """
 **Option 4 : Extended Boltzmann-Arrhenius function**
 
-| Parameter | Meaning                                               |
-|:----------|:------------------------------------------------------|
-| temp      | temperature range (Kelvin)                            |
-| p0        | scaling coefficient                                   |
-| E         | activation energy                                     |
-| Ed        | deactivation energy                                   |
-| topt      | temperature at which trait value is maximal           |
-| m         | body mass                                             |
-| beta      | allometric exponent                                   |
-| k         | Boltzmann constant (k=8.617e-5)                       |
+| Parameter     | Meaning                                               |
+|:--------------|:------------------------------------------------------|
+| temp          | temperature range (Kelvin)                            |
+| p0            | scaling coefficient                                   |
+| E             | activation energy                                     |
+| Ed            | deactivation energy                                   |
+| topt          | temperature at which trait value is maximal           |
+| p[:bodymass]  | body mass                                             |
+| beta          | allometric exponent                                   |
+| k             | Boltzmann constant (k=8.617e-5)                       |
 
 Parameters are for instance:
 
@@ -145,25 +142,24 @@ p0=0.2e12
 E=0.65
 Ed=0.72
 topt=295
-m=1
+p[:bodymass]=1
 beta=-0.25
 
 
 """
 
-function extended_BA(para_extended_BA)
+function extended_BA(para_extended_BA, p)
     temp=para_extended_BA[1]
     p0=para_extended_BA[2]
     E=para_extended_BA[3]
     Ed=para_extended_BA[4]
     topt=para_extended_BA[5]
-    m=para_extended_BA[6]
-    beta=para_extended_BA[7]
+    beta=para_extended_BA[6]
     k=8.617e-5 # Boltzmann constant
                                              |
 
     lT=1/(1+exp(-1/(k*temp)*(Ed-(Ed/topt+k*log(E/(Ed-E)))*temp)))
-    return p0*m^beta*exp(-E/(k*temp))*lT
+    return p0*p[:bodymass]^beta*exp(-E/(k*temp))*lT
 
 end
 
@@ -171,36 +167,35 @@ end
 """
 **Option 5 : Gaussian function**
 
-| Parameter | Meaning                                        |
-|:----------|:-----------------------------------------------|
-| temp      | temperature range (Kelvin)                     |
-| p0        | minimal/maximal trait value                    |
-| s         | performance breath (width of function)         |
-| topt      | temperature at which trait value is maximal    |
-| m         | body mass                                      |
-| beta      | allometric exponent                            |
+| Parameter    | Meaning                                        |
+|:-------------|:-----------------------------------------------|
+| temp         | temperature range (Kelvin)                     |
+| p0           | minimal/maximal trait value                    |
+| s            | performance breath (width of function)         |
+| topt         | temperature at which trait value is maximal    |
+| p[:bodymass] | body mass                                      |
+| beta         | allometric exponent                            |
 
 Parameters are for instance:
 
 p0=0.5
 s=20
 topt=295
-m=1
+p[:bodymass]=1
 beta=-0.25
 
 """
 
-function gaussian(para_gaussian, Ushape=0)
+function gaussian(para_gaussian, p, Ushape=0)
     temp=para_gaussian[1]
     p0=para_gaussian[2]
     s=para_gaussian[3]
     topt=para_gaussian[4]
-    m=para_gaussian[5]
-    beta=para_gaussian[6]
+    beta=para_gaussian[5]
 
     if Ushape == 0
-        return m^beta*p0*exp(-(temp-topt)^2/(2*s^2))
+        return p[:bodymass].^beta.*p0.*exp.(-(temp-topt)^2/(2*s^2))
     elseif Ushape == 1
-        return  m^beta*p0*exp((temp-topt)^2/(2*s^2))
+        return  p[:bodymass].^beta.*p0.*exp.((temp-topt)^2/(2*s^2))
     end
 end
--- src/temperature_parameters.jl
+++ src/temperature_parameters.jl
@@ -25,3 +25,44 @@ Here we define the parameters for each of these functions
 """
 
 a=Array{Float64,1}(S)
+
+"""
+**Option 3 : Exponential Boltzmann-Arrhenius function**
+
+| Parameter | Meaning                               |
+|:----------|:--------------------------------------|
+| temp      | temperature range (Kelvin)            |
+| p0        | scaling coefficient                   |
+| E         | activation energy                     |
+| m         | body mass                             |
+| beta      | allometric exponent                   |
+| k         | Boltzmann constant (k=8.617e-5)       |
+
+Parameters are for instance:
+
+p0=0.2e11
+E=0.65
+m=1
+beta=-0.25
+
+"""
+
+## Metabolic rate
+
+p0_metabolicRate_producer=-16.54
+p0_metabolicRate_herbivore=-16.54
+p0_metabolicRate_carnivore=-16.54
+
+p0_metabolicRate=zeros(S)
+p0_metabolicRate[p[:is_producer]]=p0_metabolicRate_producer
+p0_metabolicRate[p[:is_herbivore]]=p0_metabolicRate_herbivore
+p0_metabolicRate[(.!p[:is_herbivore]) .& (.!p[:is_producer])]=p0_metabolicRate_carnivore
+
+E_metabolicRate_producer=-0.69
+E_metabolicRate_herbivore=-0.69
+E_metabolicRate_carnivore=-0.69
+
+E_metabolicRate=zeros(S)
+E_metabolicRate[p[:is_producer]]=E_metabolicRate_producer
+E_metabolicRate[p[:is_herbivore]]=E_metabolicRate_herbivore
+E_metabolicRate[(.!p[:is_herbivore]) .& (.!p[:is_producer])]=E_metabolicRate_carnivore
