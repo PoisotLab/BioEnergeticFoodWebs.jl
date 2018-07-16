e31ce3e083e9dce22db92fa4e49f045efc1e0059
Azenor Bideault <azenor.bideault@usherbrooke.ca>
2018-07-16 17:05:38 -0400

    Remove redondant file temperature_dependence

--- src/temperature_parameters.jl
+++ src/temperature_parameters.jl
@@ -1,5 +1,5 @@
 """
-**Functions of thermal performance curve for model parameters**
+**Define parameters of thermal dependence functions**
 
 We included different functions of temperature dependence : 1) Extended Eppley function
                                                             2) Quadratic function
@@ -7,8 +7,21 @@ We included different functions of temperature dependence : 1) Extended Eppley f
                                                             4) Extended Boltzmann-Arrhenius function (Johnson-Lewin)
                                                             5) Gaussian (inverted Gaussian) function
 
-In each case, the function returns the biological rate value at a given temperature.
+Here we define the parameters for each of these functions
 
 """
 
 """
+**Option 1 : Extended Eppley function**
+
+| Parameter | Meaning                                                           |
+|:----------|:------------------------------------------------------------------|
+| temp      | temperature (Celsius degree)                                      |
+| a         | parameter from the Eppley curve for the growth phase              |
+| b         | parameter from the Eppley curve for the growth phase              |
+| z         | location of the maximum of the quadratic portion of the function  |
+| w         | thermal breadth                                                   |
+
+"""
+
+a=Array{Float64,1}(S)
