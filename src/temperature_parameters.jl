ea9ec0472ce0e9ac81ac38300833d79ad171d8c1
Azenor Bideault <azenor.bideault@usherbrooke.ca>
2018-07-16 19:18:39 -0400

    add temperature_parameters

--- src/temperature_parameters.jl
+++ src/temperature_parameters.jl
@@ -29,14 +29,14 @@ a=Array{Float64,1}(S)
 """
 **Option 3 : Exponential Boltzmann-Arrhenius function**
 
-| Parameter | Meaning                               |
-|:----------|:--------------------------------------|
-| temp      | temperature range (Kelvin)            |
-| p0        | scaling coefficient                   |
-| E         | activation energy                     |
-| m         | body mass                             |
-| beta      | allometric exponent                   |
-| k         | Boltzmann constant (k=8.617e-5)       |
+| Parameter     | Meaning                               |
+|:--------------|:--------------------------------------|
+| temp          | temperature range (Kelvin)            |
+| p0            | scaling coefficient                   |
+| E             | activation energy                     |
+| p[:bodymass]  | body mass                             |
+| beta          | allometric exponent                   |
+| k             | Boltzmann constant (k=8.617e-5)       |
 
 Parameters are for instance:
 
@@ -47,7 +47,7 @@ beta=-0.25
 
 """
 
-## Metabolic rate
+# Metabolic rate
 
 p0_metabolicRate_producer=-16.54
 p0_metabolicRate_herbivore=-16.54
@@ -66,3 +66,17 @@ E_metabolicRate=zeros(S)
 E_metabolicRate[p[:is_producer]]=E_metabolicRate_producer
 E_metabolicRate[p[:is_herbivore]]=E_metabolicRate_herbivore
 E_metabolicRate[(.!p[:is_herbivore]) .& (.!p[:is_producer])]=E_metabolicRate_carnivore
+
+beta_metabolicRate_producer=-0.31
+beta_metabolicRate_herbivore=-0.31
+beta_metabolicRate_carnivore=-0.31
+
+beta_metabolicRate=zeros(S)
+
+beta_metabolicRate[p[:is_producer]]=beta_metabolicRate_producer
+beta_metabolicRate[p[:is_herbivore]]=beta_metabolicRate_herbivore
+beta_metabolicRate[(.!p[:is_herbivore]) .& (.!p[:is_producer])]=beta_metabolicRate_carnivore
+
+# Growth rate
+
+# Functional response
