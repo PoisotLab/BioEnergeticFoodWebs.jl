367ab51215f522469c42d02e61e8feda233750f2
Timothée Poisot <timothee.poisot@umontreal.ca>
2018-07-18 11:49:03 -0400

    :hand:

--- tests.jl
+++ tests.jl
@@ -2,10 +2,10 @@ include("src/BioEnergeticFoodWebs.jl")
 using BioEnergeticFoodWebs
 using Plots
 
-S = 55
+S = 15
 A = nichemodel(S, 0.2)
 parameters = model_parameters(A)
-bm = rand(Float64, S).*2.001
+bm = rand(Float64, S).*(1/S)
 
 @time sim = simulate(parameters, bm; stop=500, use=:stiff)
 
@@ -13,4 +13,4 @@ minimum(sim[:B])
 
 pl2 = vec(sum(sim[:B].>0.0, 2)) |> x -> plot(sim[:t], x, leg=false)
 pl1 = plot(sim[:t], sim[:B], leg=false, c=:teal)
-plot(pl2, pl1)
+plot(pl1, pl2)
