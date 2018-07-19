7f0f1020c0844f88c905ce4891454c403bd03217
Eva Delmas <eva.delmas@umontreal.ca>
2018-07-19 11:23:24 -0400

    :bug: define Γ before calling it ...

--- src/make_parameters.jl
+++ src/make_parameters.jl
@@ -263,6 +263,7 @@ function model_parameters(A; K::Float64=1.0, Z::Float64=1.0,
   # Step 17 -- Half-saturation constant
   Γ = 1 ./ (attack_r .* handling_t)
   Γ[isnan.(Γ)] = 0.0
+  p[:Γ] = Γ
 
   # Step 18 -- Efficiency matrix
   get_efficiency(p)
