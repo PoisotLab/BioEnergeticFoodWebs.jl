9d1d779ffe303a91494f436568ffc8eb96ad5e4a
Azenor Bideault <azenor.bideault@usherbrooke.ca>
2018-11-01 13:22:14 -0400

    Export temperature_size_rule function

--- src/BioEnergeticFoodWebs.jl
+++ src/BioEnergeticFoodWebs.jl
@@ -51,5 +51,6 @@ include(joinpath(".", "rewiring/parameters/checkParameters.jl"))
 include(joinpath(".", "rewiring/parameters/updateParameters.jl"))
 
 include(joinpath(".", "temperature_dependence_functions.jl"))
+include(joinpath(".", "temperature_size_rule.jl"))
 
 end
