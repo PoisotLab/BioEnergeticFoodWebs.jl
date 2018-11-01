0f9e700c76fc80539c71b039d46caa7bf2579cf8
Azenor Bideault <azenor.bideault@usherbrooke.ca>
2018-11-01 12:11:14 -0400

    :sparkles: Temperature_size_rule function

--- src/TSRfunction.jl
+++ src/TSRfunction.jl
@@ -1,81 +1,35 @@
 ### Temperature Size Rule, following Forster et al 2012 ###
 
-## Mean aquatic response : average TS response for aquatic Metazoa
+# Args:
+# dry_mass_20 temperature in Kelvin degrees, DM20 dry mass at 20 degrees
 
-# Args: temperature in Celsius degrees, DM20 dry mass at 20 degrees
+function temperature_size_rule(dry_mass_20, temperature_K, TSR_type)
 
-function TSR_aqua(temp,DM20)
-    # tempCel=temp-273.15
-    c=6.5
-    PCM=-3.90-0.53*log10(DM20)     # Percentage change in body-mass per degrés C
-    S=log(PCM/100+1)               # Sign and magnitude of TS response
-    M=c*DM20.*exp.(S.*(temp-20))./1000 # dry mass converted to fresh mass in g
-    return(M)
+    temperature_C = temperature_K - 273.15
+
+    if TSR_type == mean_aquatic
+        PCM = -3.90-0.53*log10(dry_mass_20)     # PCM = Percentage change in body-mass per degrés C
+
+    elseif TSR_type == mean_terrestrial
+        PCM = -1.72+0.54*log10(dry_mass_20)
+
+    elseif TSR_type == maximum
+        PCM = -8
+
+    elseif TSR_type == reverse
+        PCM = 4
+
+    elseif TSR_type == no_response
+        PCM = 0
+
+    else
+        println("TSR_type should be one of : mean_aquatic, mean_terrestrial, maximum, reverse or no_response")
+
+    end
+
+    convert_dfmass = 6.5
+    TS_response = log(PCM/100+1)               # Sign and magnitude of TS response
+    fresh_mass = convert_dfmass*dry_mass_20*exp(TS_response*(temperature_C-20))  # dry mass converted to fresh mass
+
+    return fresh_mass
 end
-
-temp=linspace(5,35,50)
-DMtsr=0.2
-
-bmTSR_aqua=TSR_aqua(temp,DMtsr)
-
-using Plots
-plotlyjs()
-
-plot(temp,bmTSR_aqua)
-
-## Mean terrestrial response : average TS response for terrestrial Metazoa
-
-function TSR_terrestrial(temp,DM20)
-    c=6.5
-    PCM=-1.72+0.54*log10(DM20)
-    S=log(PCM/100+1)
-    M=c*DM20*exp(S*(temp-20))/1000
-    return(M)
-end
-
-temp=linspace(5,35,50)
-DMtsr=0.2
-
-bmTSR_terrestrial=TSR_terrestrial(temp,DMtsr)
-
-using Plots
-plotlyjs()
-
-plot(temp,bmTSR_terrestrial)
-
-## Max TS response : strongest negative response
-
-function TSR_max(temp,DM20)
-    c=6.5
-    PCM=-8
-    S=log(PCM/100+1)
-    M=c*DM20*exp(S*(temp-20))/1000
-    return(M)
-end
-
-temp=linspace(5,35,50)
-DMtsr=0.2
-
-bmTSR_max=TSR_max(temp,DMtsr)
-
-using Plots
-plotlyjs()
-
-plot(temp,bmTSR_max)
-
-## Reverse TS response : strongest positive TS response
-
-function TSR_reverse(temp,DM20)
-    c=6.5
-    PCM=4
-    S=log(PCM/100+1)
-    M=c*DM20*exp(S*(temp-20))/1000
-    return(M)
-end
-
-temp=linspace(5,35,50)
-DMtsr=0.2
-
-bmTSR_reverse=TSR_reverse(temp,DMtsr)
-
-lines(temp,bmTSR_reverse)
