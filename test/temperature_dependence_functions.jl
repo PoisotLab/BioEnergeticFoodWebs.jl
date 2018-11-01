0225a1d6875d4fe78898544e72f1c72b7314675c
Eva Delmas <eva.delmas@umontreal.ca>
2018-11-01 13:51:59 -0400

    :white_check_mark: Exponentia Boltzmann Arrhenius

--- test/temperature_dependence_functions.jl
+++ test/temperature_dependence_functions.jl
@@ -18,3 +18,24 @@ module TestExtendedEppley
     @test testvalues[2] ≈ -0.843 atol = 0.001
     @test testvalues[3] == testvalues[1]
 end
+
+module TestExponentialBA
+    using BioEnergeticFoodWebs
+    using Base.Test
+
+    A = [0 1 0 ; 0 0 0 ; 0 1 0]
+    p = model_parameters(A)
+    BAfunction = exponential_BA(@NT(norm_constant = -16.54, activation_energy = -0.69, T0 = 293.15, β = -0.31))
+    body_size_relative = p[:bodymass]
+    testvalues = BAfunction(body_size_relative, 273.15, p)
+    @test testvalues[1] ≈ -122.216 atol = 0.001
+    @test testvalues[2] ≈ -122.216 atol = 0.001
+    @test testvalues[3] ≈ -122.216 atol = 0.001
+    @test length(testvalues) == size(A,1)
+
+    body_size_relative = [10.0, 1.0, 10.0]
+    testvalues = BAfunction(body_size_relative, 273.15, p)
+    @test testvalues[1] ≈ -59.859 atol = 0.001
+    @test testvalues[2] ≈ -122.216 atol = 0.001
+    @test testvalues[3] == testvalues[1]
+end
