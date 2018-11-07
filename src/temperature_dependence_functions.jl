bc79ceca15a3cfc068bb71a103128c6ad22be7ce
Azenor Bideault <azenor.bideault@usherbrooke.ca>
2018-11-07 10:33:22 -0500

    :sparkles: exponential_BA_x

--- src/temperature_dependence_functions.jl
+++ src/temperature_dependence_functions.jl
@@ -265,7 +265,15 @@ metabolicrate=exponential_BA(@NT(norm_constant = -16.54, activation_energy = -0.
 
 function exponential_BA_x(T_param)
     k=8.617e-5
-    return (bodymass, T, p) -> T_param.norm_constant .* (bodymass .^T_param.β) .* exp.(T_param.activation_energy .* (T .- T_param.T0) ./ (k * T .* T_param.T0))
+
+    return (bodymass, T, p) -> for i in 1:1
+                                norm_constant_all = T_param.norm_constant_producer .* p[:is_producer] .+ T_param.norm_constant_vertebrate .* p[:vertebrates] .+ T_param.norm_constant_invertebrate .* (.!p[:vertebrates] .& .!p[:is_producer])
+                                activation_energy_all = T_param.activation_energy_producer .* p[:is_producer] .+ T_param.activation_energy_vertebrate .* p[:vertebrates] .+ T_param.activation_energy_invertebrate .* (.!p[:vertebrates] .& .!p[:is_producer])
+                                T0_all = T_param.T0_producer .* p[:is_producer] .+ T_param.T0_vertebrate .* p[:vertebrates] .+ T_param.T0_invertebrate .* (.!p[:vertebrates] .& .!p[:is_producer])
+                                β_all = T_param.β_producer .* p[:is_producer] .+ T_param.β_vertebrate .* p[:vertebrates] .+ T_param.β_invertebrate .* (.!p[:vertebrates] .& .!p[:is_producer])
+
+                                return norm_constant_all .* (bodymass .^β_all) .* exp.(activation_energy_all .* (T .- T0_all) ./ (k * T .* T0_all))
+                            end
 end
 
 """
@@ -352,6 +360,35 @@ function exponential_BA_attackr(T_param)
                             end
                             return norm_constant_all .* (bodymass .^β_all) .* exp.(activation_energy_all .* (T .- T0_all) ./ (k * T .* T0_all))
                         end
+end
+
+"""
+**Option 2 : Exponential Boltzmann-Arrhenius function for handling time**
+
+This function can be called as an argument in `model_parameters` to define an exponential Boltzmann-Arrhénius function (Gillooly et al. 2001, Brown et al. 2004) for one of:
+    - metabolic rate
+    - producers growth rate
+    - attack rate
+    - handling time (not recommended as it is a hump-shaped curve)
+
+
+| Parameter         | Meaning                               | Default values | Reference                             |
+|:------------------|:--------------------------------------|:---------------|:--------------------------------------|
+| norm_constant     | scaling coefficient                   | -16.54         | Ehnes et al. 2011, Binzer et al. 2012 |
+| activation_energy | activation energy                     | -0.69          | Ehnes et al. 2011, Binzer et al. 2012 |
+| T0                | normalization temperature (K)         | 293.15         | Binzer et al. 2012, Binzer et al. 2012|
+| β                 | allometric exponent                   | -0.31          | Ehnes et al. 2011                     |
+
+Default values are given as an example for metabolic rate x.
+
+Example:
+metabolicrate=exponential_BA(@NT(norm_constant = -16.54, activation_energy = -0.69, T0 = 293.15, β = -0.31))
+
+"""
+
+function exponential_BA_handlingt(T_param)
+    k=8.617e-5
+    return (bodymass, T, p) -> T_param.norm_constant .* (bodymass .^T_param.β) .* exp.(T_param.activation_energy .* (T .- T_param.T0) ./ (k * T .* T_param.T0))
 end
 
 
