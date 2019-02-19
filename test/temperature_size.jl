f01b5b6042119822e9a4ec852e74e5b17356c883
Eva Delmas <eva.delmas@umontreal.ca>
2019-02-19 17:04:53 -0500

    :white_check_mark: Mean aquatic temperature effect on bodymass

--- test/temperature_size.jl
+++ test/temperature_size.jl
@@ -26,14 +26,29 @@ module TestNoEffectTempSize
     @test p_dm[:bodymass] == expected_mass_dm
 end
 
-# module TestEffectTempSize
-#     using BioEnergeticFoodWebs
-#     using Base.Test
-#
-#     # MEAN AQUATIC
-#
-#     # MEAN TERRESTRIAL
-#
-#     # MAXIMUM
-#
-# end
+module TestEffectTempSize
+    using BioEnergeticFoodWebs
+    using Base.Test
+
+    EC = [0 0 1 ; 0 0 1 ; 0 0 0]
+    LFC = [0 1 0 ; 0 0 1 ; 0 0 0]
+    dm = [1.0, 0.8, 0.2]
+    temp = 295.0
+    temp_c = temp-273.15
+    temp2 = 285.0
+    temp2_c = temp2-273.15
+
+    # MEAN AQUATIC
+    p_aqua_1 = model_parameters(EC, TSR_type = :mean_aquatic, dry_mass_293 = dm, T = temp)
+    pcm_aqua_1 = -3.90 .- 0.53 .* log10.(dm)
+    TSr_aqua_1 = log.(pcm_aqua_1 ./ 100 .+ 1)
+    expected_bm_aqua = (dm .* 6.5) .* exp.(TSr_aqua_1 .* (temp_c-20))
+    @test p_aqua_1[:bodymass] == expected_bm_aqua
+    p_aqua_2 = model_parameters(EC, TSR_type = :mean_aquatic, dry_mass_293 = dm, T = temp2)
+    @test p_aqua_2[:bodymass] > p_aqua_1[:bodymass]
+    
+    # MEAN TERRESTRIAL
+
+    # MAXIMUM
+
+end
