9fffdb35777df719eba4ce6df028b6bb6456097b
Eva Delmas <eva.delmas@umontreal.ca>
2019-02-21 14:58:07 -0500

    :zap: Identify overlap species for Staniczencko rewiring

--- src/rewiring/StaniczenkoRewire.jl
+++ src/rewiring/StaniczenkoRewire.jl
@@ -3,23 +3,29 @@
 
 (Based on Staniczenko et al., 2010)
 This function calculate the rewiring graph R from the adjacency matrix A.
-The rewiring graph indicates biologically plausible trophic rewiring.
+The rewiring graph indicates overlap species:
 R[i,j] = 1 if i and j share at least one predator AND i has at least one predator
 that does not prey on j (i is in that case an overlap species).
 Used internally by rewire().
 """
-function rewiring_graph(A)
-  S = size(A, 1)
+function rewiring_graph(parameters)
+  S = size(parameters[:A], 1)
   R = convert(Array{Int64,2}, zeros(S, S)) #pre-allocate
-  Abis = copy(A)
-  [Abis[i,i] = 0 for i in 1:S]
-  A_slices = map(x -> Abis[:,x], collect(1:S))
-  for i in 1:S
-    shared_predators = map(x -> sum(A_slices[i] .== x .== 1) >= 1, A_slices)
-    unshared_predators = map(x -> sum((A_slices[i] .== 1) .& (x .== 0)) >= 1, A_slices)
-    is_overlap = map((x,y) -> x .== y .== true, shared_predators, unshared_predators)
-    idx = find(is_overlap .== true)
-    R[i, idx] = 1
+  Atmp = deepcopy(A)
+  links = findall(!iszero, Atmp)
+  consumers = [links[x][1] for x = 1:length(links)]
+  unique_consumers = unique(consumers)
+  resources = [links[x][2] for x = 1:length(links)]
+  unique_resources = unique(resources)
+  for r1 in unique_resources #for each unique resource
+    c1 = consumers[resources .== r1]
+    if length(c1) > 1
+      for r2 in unique_resources
+        c2 = consumers[resources .== r2]
+        shared_cons = [x ∈ c2 for x = c1]
+        (sum(shared_cons) > 0) & (sum(.!shared_cons) > 0) ? R[r1,r2] = 1 : nothing
+      end
+    end
   end
   return R
 end
@@ -36,7 +42,7 @@ function potential_newlinks(A, R, parameters)
   R_slices = [R[:, i] for i in 1:S]
   #identify "overlap species" = species that share at least one
   #consumer but also have 1 unshared consumer (results in consumers competition)
-  overlap_species = map(x -> find(x .== 1), R_slices)
+  overlap_species = map(x -> findall(x .== 1), R_slices)
   sp = convert(BitArray{1}, [overlap_species[x] != Int64[] for x in 1:size(overlap_species,1)])
   #identify overlap species consumers
   potential_predators = map(x -> A[:,x], overlap_species)[sp]
@@ -75,7 +81,7 @@ function Staniczenko_rewire(parameters)
   #identify extinct species
   Ɛ = parameters[:extinctions]
   #identify potential new links
-  R = rewiring_graph(A)
+  R = rewiring_graph(parameters)
   P = potential_newlinks(A, R, parameters)
   #keep those that contain one of the released preys
   released_preys = unique(findn(A[Ɛ,:])[2])
