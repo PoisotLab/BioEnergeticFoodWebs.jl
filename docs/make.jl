b332f65d8e8e5ca8ca8c4eaeff3b45b0a1495a19
Eva Delmas <eva.delmas@umontreal.ca>
2019-03-01 17:10:54 -0500

    (same) remove julia keyword in deploydocs

--- docs/make.jl
+++ docs/make.jl
@@ -8,11 +8,19 @@ using BioEnergeticFoodWebs
 
 makedocs(
          modules = [BioEnergeticFoodWebs],
-         sitename = "BioEnergeticFoodWebs.jl"
+         sitename = "BioEnergeticFoodWebs.jl",
+         pages = [
+                "Index" => "index.md",
+                "Installation" => "man/installation.md",
+                "Generating random networks" => "man/random.md",
+                "First simulation" => "man/first_simulation.md",
+                "Extinctions" => "man/extinctions.md",
+                "Temperature dependence" => "man/temperature.md",
+                "Contributing" => "man/contributing.md"
+         ]
         )
 
 deploydocs(
            deps = Deps.pip("pygments", "mkdocs==0.17.5", "mkdocs-material==2.9.4", "python-markdown-math"),
-           julia = "0.7",
            repo = "github.com/PoisotLab/BioEnergeticFoodWebs.jl.git"
           )
