51bf150fcf142934103cc2fe52cd40a8050b846d
Eva Delmas <eva.delmas@umontreal.ca>
2019-03-04 14:19:51 -0500

    try to fix docs deployment

--- docs/make.jl
+++ docs/make.jl
@@ -13,6 +13,5 @@ makedocs(
 
 deploydocs(
            deps = Deps.pip("pygments", "mkdocs==0.17.5", "mkdocs-material==2.9.4", "python-markdown-math"),
-           julia = "nightly",
            repo = "github.com/PoisotLab/BioEnergeticFoodWebs.jl.git"
           )
