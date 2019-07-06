2e9faf4f6fe7f49388e2d88a6841691219a67c82
Eva Delmas <eva.delmas@gmail.com>
2019-07-06 11:20:50 -0400

    test in module

--- test/mortality.jl
+++ test/mortality.jl
@@ -1,26 +1,29 @@
-using BioEnergeticFoodWebs
-using Test
+module TestDDMortality
+    using BioEnergeticFoodWebs
+    using Test
 
-A = [0 1 0 ; 0 0 1 ; 0 0 0]
-ddm = (x -> x .* 0.2)
-p = model_parameters(A, d = ddm)
+    A = [0 1 0 ; 0 0 1 ; 0 0 0]
+    ddm = (x -> x .* 0.2)
+    p = model_parameters(A, d = ddm)
 
-b = [0.5, 0.6, 0.8]
-PI_death = BioEnergeticFoodWebs.density_dependent_mortality(p, b)
-@test all(PI_death .== ddm(b) .* Int.(.!p[:is_producer]))
+    b = [0.5, 0.6, 0.8]
+    PI_death = BioEnergeticFoodWebs.density_dependent_mortality(p, b)
+    @test all(PI_death .== ddm(b) .* Int.(.!p[:is_producer]))
 
-dbdt = BioEnergeticFoodWebs.dBdt(zeros(3), b, p, 1)
+    dbdt = BioEnergeticFoodWebs.dBdt(zeros(3), b, p, 1)
 
-# Consumption
-gain, loss = BioEnergeticFoodWebs.consumption(p, b)
+    # Consumption
+    gain, loss = BioEnergeticFoodWebs.consumption(p, b)
 
-# Growth
-growth, G = BioEnergeticFoodWebs.get_growth(p, b)
+    # Growth
+    growth, G = BioEnergeticFoodWebs.get_growth(p, b)
 
-# Balance
-balance = zeros(eltype(b), length(b))
-for i in eachindex(balance)
-  balance[i] = growth[i] + gain[i] - loss[i] - PI_death[i]
+    # Balance
+    balance = zeros(eltype(b), length(b))
+    for i in eachindex(balance)
+      balance[i] = growth[i] + gain[i] - loss[i] - PI_death[i]
+    end
+
+    @test all(dbdt .== balance)
+
 end
-
-@test all(dbdt .== balance)
