76d696839b9f36c631d1756fa7be132536754d38
Eva Delmas <eva.delmas@gmail.com>
2020-10-23 14:40:57 -0400

    :bug: Fix some more tests

--- test/rewiring/GilljamRewire.jl
+++ test/rewiring/GilljamRewire.jl
@@ -9,7 +9,7 @@ module TestGilljamRewire
          1 0 0 ;
          1 1 1 ]
     biomass = [1.0,1.0,1.0]
-    parameters = model_parameters(A, rewire_method = :DS)
+    parameters = BioEnergeticFoodWebs.model_parameters(A, rewire_method = :DS)
 
 #testing Gilljam no extinctions
     GilljamTest = BioEnergeticFoodWebs.Gilljam(S,parameters,biomass)
@@ -18,7 +18,10 @@ module TestGilljamRewire
 #Testing with extinction
     parameters[:extinctions] = 1
     GilljamTest = BioEnergeticFoodWebs.Gilljam(S,parameters,biomass)
-    @test GilljamTest[1] == [0 0 0; 0 1 0; 0 1 1]
-    parameters[:costMat][2,2] = 0.0
-    @test GilljamTest[2] == parameters
+    @test (GilljamTest[1] == [0 0 0; 0 1 0; 0 1 1]) | (GilljamTest[1] == ([0 0 0; 0 0 1; 0 1 1]))
+    p1 = deepcopy(parameters)
+    p2 = deepcopy(parameters)
+    p1[:costMat][2,2] = 0.0
+    p2[:costMat][2,3] = 0.0
+    @test (GilljamTest[2] == p1) | (GilljamTest[2] == p2)
 end
